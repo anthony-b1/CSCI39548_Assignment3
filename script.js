459411999c1dbc018eaa6829d9182d52ae141d59
anthony-b1 <anthony78510@gmail.com>
2022-06-20 14:16:05 -0400

    AddCol Function

--- script.js
+++ script.js
@@ -5,12 +5,30 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    alert("Clicked Add Row"); // Replace this line with your code.
+    let tbl = document.getElementById('grid'); // table reference
+    let row = tbl.insertRow(tbl.rows.length)
+    if (tbl.rows[0].cells.length == 0){           // If there is no existing cells, insert one cell
+        row.insertCell(0);
+    }
+    else {
+        for (let i = 0; i < tbl.rows[0].cells.length; i++) {    // Check the number of columns on the first row 
+            row.insertCell(i);                                  // And make the same number of cells in a new row 
+        }
+    }
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+    let tbl = document.getElementById("grid");
+    if (tbl.rows.length == 0){                  // If there is no existing cells:
+        let row = tbl.insertRow();              // Insert a row 
+        row.insertCell(0);                      // And a cell
+    }
+    else{
+        for (let i = 0; i < tbl.rows.length; i++) {                         // Check the number of rows 
+            tbl.rows[i].insertCell(tbl.rows[i].cells.length);  // Make a column with the length equal to the number of rows 
+        }
+    }
 }
 
 // Remove a row
